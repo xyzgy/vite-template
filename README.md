1457c9891e0a21a5a467cf26b477dfef7fb0f8d9
guoyan <xyzgy@xyzgy.xyz>
2022-07-20 11:13:00 +0800

    url格式化

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## vite + pinia + vue-router + axios + element-plus + vant
 * pinia 替代 vuex
 * hooks 替代 mixins
-* axios封装
+* axios封装 pending存储更新请求状态
 * loading 基于hooks封装  
 * 公共头部组件，支持路由自动切换
 * pc与mobile判断
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,56 @@ export const isMobile = () => {
         /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
     );
 }
+
+// 对象转URL参数 https://www.uviewui.com/js/queryParams.html
+export const queryParams = (data = {}, isPrefix = true, arrayFormat = 'brackets') => {
+	const prefix = isPrefix ? '?' : ''
+	const _result = []
+	if (['indices', 'brackets', 'repeat', 'comma'].indexOf(arrayFormat) == -1) arrayFormat = 'brackets'
+	for (const key in data) {
+		const value = data[key]
+		// 去掉为空的参数
+		if (['', undefined, null].indexOf(value) >= 0) {
+			continue
+		}
+		// 如果值为数组，另行处理
+		if (value.constructor === Array) {
+			// e.g. {ids: [1, 2, 3]}
+			switch (arrayFormat) {
+				case 'indices':
+					// 结果: ids[0]=1&ids[1]=2&ids[2]=3
+					for (let i = 0; i < value.length; i++) {
+						_result.push(`${key}[${i}]=${value[i]}`)
+					}
+					break
+				case 'brackets':
+					// 结果: ids[]=1&ids[]=2&ids[]=3
+					value.forEach((_value) => {
+						_result.push(`${key}[]=${_value}`)
+					})
+					break
+				case 'repeat':
+					// 结果: ids=1&ids=2&ids=3
+					value.forEach((_value) => {
+						_result.push(`${key}=${_value}`)
+					})
+					break
+				case 'comma':
+					// 结果: ids=1,2,3
+					let commaStr = ''
+					value.forEach((_value) => {
+						commaStr += (commaStr ? ',' : '') + _value
+					})
+					_result.push(`${key}=${commaStr}`)
+					break
+				default:
+					value.forEach((_value) => {
+						_result.push(`${key}[]=${_value}`)
+					})
+			}
+		} else {
+			_result.push(`${key}=${value}`)
+		}
+	}
+	return _result.length ? prefix + _result.join('&') : ''
+}
